fed00ba6ac90b7b216faf6c1e96d5831ca86bff6
Joseph Baca <josephcbaca@gmail.com>
2020-08-07 19:52:17 -0700

    Created YES db.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function Header() {
     return (
       <header  className="header header-style">
-        <h1 class="heading-style">Fela Kuti</h1>
+        <h1 class="heading-style">YES</h1>
       </header>
     );
   }
