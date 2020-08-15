24df2621445766dad56a9fdd4e89790e0c1b4a69
Joseph Baca <josephcbaca@gmail.com>
2020-08-15 15:45:54 -0700

    Completed order state for three sorts original, asc, desc.

--- src/components/ListRows/index.js
+++ src/components/ListRows/index.js
@@ -6,21 +6,41 @@ import "./style.css";
 function ListRows(props) {
     // Place to keep state of sorted list
     const [listRows, setListRows] = useState(props)
-
+    const [order, setOrder] = useState(0)
 
 
 
     // Function to complete sorting and return sorted list
     function handleSortListYear() {
-        setListRows(listRows).sort((a, b) => a.year_joined < b.year_joined)
-        //   if (a.year_joined < b.year_joined) {
-        //     return -1;
-        //   }
-        //   if (a.year_joined > b.year_joined) {
-        //     return 1;
-        //   }
-        //   return 0;
-        // });
+        const orderList = ["original", "asc", "desc"]
+        let newOrder = order + 1
+
+        if (newOrder === orderList.indexOf("asc")) {
+            // setListRows(listRows.sort((a, b) => a - b ))
+            setOrder(newOrder)
+        }
+        if (newOrder === orderList.indexOf("desc")) {
+            // setListRows(listRows.sort((a, b) => a - b ))
+
+            setOrder(newOrder)
+        }
+        if(newOrder === orderList.length) {
+            setOrder(0)
+        }
+
+
+        // listRows.sort((a, b) =>
+        // if (a.year_joined < b.year_joined) {
+
+        // }
+        // //   if (a.year_joined < b.year_joined) {
+        // //     return -1;
+        // //   }
+        // //   if (a.year_joined > b.year_joined) {
+        // //     return 1;
+        // //   }
+        // //   return 0;
+        // // });
     }
 
     // Click handler that calls sortin function and updates state
@@ -29,7 +49,7 @@ function ListRows(props) {
         <div>
             <FormBtn
                 onClick={handleSortListYear}
-              >
+            >
                 Sort by Year
               </FormBtn>
             <ListHeader />
